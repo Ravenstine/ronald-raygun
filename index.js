2cfe8fcd1205e3cefb1e7950aca24345c5b075bf
Ben Titcomb <btitcomb@scpr.org>
2017-05-23 20:44:53 -0700

    Separate mp3 code into its own module and delete wav code.

--- index.js
+++ index.js
@@ -3,74 +3,54 @@
 const express      = require('express');
 const fs           = require('fs');
 const https        = require('https');
-const wav          = require('./lib/node-wav');
-const waveheader   = require("waveheader");
-const RewindBuffer = require('./lib/rewind-buffer');
+const Mp3Source    = require('./lib/mp3-source');
 
-let rewindBuffer = new RewindBuffer({maxFrames: 300});
 let app          = express();
 let httpsServer  = https.createServer({
   key: fs.readFileSync('./key.pem', 'utf8'),
   cert: fs.readFileSync('./cert.pem', 'utf8'),
   passphrase: "password"
 }, app);
-var expressWs    = require('express-ws')(app, httpsServer);
- 
-app.get('/stream', function (req, res) {
-  res.writeHead(200, {'Content-Type': 'audio/wav'});
-  res.write(waveheader(44100 * 6000, {bitDepth: 16}), 'binary');
-  let offset      = req.query.offset ? parseInt(req.query.offset) : 0;
+
+let sources = {
+  mp3: new Mp3Source({
+    hostname: 'live.scpr.org',
+    protocol: 'https:',
+    path: '/kpcclive?ua=SCPRWEB&preskip=true',
+    maxFrames: 7200
+  })
+};
+
+app.get('/stream/:type', function (req, res) {
+  res.writeHead(200, {'Content-Type': 'audio/mpeg'});
+  let offset      = (req.query.offset ? parseInt(req.query.offset) : 0) + 8;
+  // ☝️ Because MP3 frames depend on previous frames, it seems like
+  // we need about 8 seconds worth of frames for the stream to start playing
+  // immediately when a zero offset is provided.  In essence, there's always
+  // going to be an 8 second delay for the tradeoff of not having to wait.
   let isConnected = true;
-  rewindBuffer.read(offset, (samples) => {
-    let encoded = wav.encode([samples], {bitDepth: 16, skipHeader: true, sampleRate: 44100});
-    res.write(encoded, 'binary');
+  sources[req.params.type].read(offset, (samples) => {
+    res.write(Buffer.from(samples), 'binary');
     return isConnected;
   });
   let terminateRead = () => {
     console.log('client disconnected');
     isConnected = false;
   }
+  // Stop the read process from continuing
+  // if the client disconnects
   req.on("close", terminateRead);
   req.on("end",   terminateRead);
 });
 
-app.get('/get-down', function(req, res){
-  let file   = fs.readFileSync('./resources/none-shall-pasadena.wav');
-  let audio  = wav.decode(file).channelData[0];
-  let len    = audio.length;
-  let i = 0;
-  while (i < len) {
-    rewindBuffer.push(audio.slice(i, i+44100));
-    i += 44100;
-  }
-  res.send('its done');
-});
-
-app.ws('/record', function(ws, req) {
-  let i         = 0;
-  let preBuffer = new Float32Array(44100);
-  ws.on('message', function(msg) {
-    // receive sample buffers and concatenate
-    // them into larger second-sized buffers,
-    // and then push those into the rewind buffer
-    let samples = new Float32Array(msg.buffer);
-    let idx     = 0;
-    let len     = samples.length;
-    while(idx<len){
-      if(!(i<44100)){
-        rewindBuffer.push(Float32Array.from(preBuffer));
-        i = 0;
-      }
-      preBuffer[i] = samples[idx];
-      i++;
-      idx++;
-    }
-  });
-});
-
 
 httpsServer.listen(3003);
-// app.listen(3003);
+
+
+
+
+
+
 
 
 
