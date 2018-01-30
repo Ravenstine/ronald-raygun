00b8f834027c139f86fad34a2f424b968a3dd37d
Ben Titcomb <btitcomb@scpr.org>
2018-01-29 20:41:12 -0800

    I worked on adding HLS but I'm pretty sure I didn't get it working

--- index.js
+++ index.js
@@ -3,8 +3,10 @@
 const express      = require('express');
 const fs           = require('fs');
 const https        = require('https');
-const Mp3Source    = require('./lib/mp3-source');
-const AACSource    = require('./lib/aac-source');
+const YAML         = require('js-yaml');
+const Source       = require('./lib/source');
+
+let config       = YAML.safeLoad(fs.readFileSync('config.yml', 'utf-8'));
 
 let app          = express();
 let httpsServer  = https.createServer({
@@ -13,31 +15,43 @@ let httpsServer  = https.createServer({
   passphrase: "password"
 }, app);
 
-let sources = {
-  mp3: new Mp3Source({
-    hostname: 'live.scpr.org',
-    protocol: 'https:',
-    path: '/kpcclive?ua=SCPRWEB&preskip=true',
-    maxFrames: 7200
-  }),
-  aac: new AACSource({
-    hostname: 'live.scpr.org',
-    protocol: 'https:',
-    path: '/aac?ua=SCPRWEB&preskip=true',
-    maxFrames: 7200
-  })
-};
+let sources = {};
+
+Object.keys(config.streams).forEach((key) => {
+  sources[key] = new Source(config.streams[key]);
+});
+
+app.get('/stream/hls', function(req, res){
+
+});
 
 app.get('/stream/:type', function (req, res) {
   let source = sources[req.params.type];
   res.writeHead(200, {'Content-Type': source.contentType});
-  let offset      = (req.query.offset ? parseInt(req.query.offset) : 0) + 8;
+  let offset      = (req.query.offset    ? parseInt(req.query.offset) : 0) + 8;
+  let iteration   = (req.query.iteration ? (parseInt(req.query.iteration) + 8) : undefined);
   // ☝️ Because MP3 frames depend on previous frames, it seems like
   // we need about 8 seconds worth of frames for the stream to start playing
   // immediately when a zero offset is provided.  In essence, there's always
   // going to be an 8 second delay for the tradeoff of not having to wait.
+  let limit       = req.query.limit ? parseInt(req.query.limit) : 0;
+  let frameCount  = 0; 
   let isConnected = true;
+  // Let's say we want to return a URL where a
+  // segment of audio can always be found as long
+  // as it exists inside our rewind buffer?  In that
+  // case, we're going to need to calculate the index
+  // of the frame we want to start with based on a 
+  // given iteration number, which corresponds with an
+  // iteration of the rewind buffer.
+  if(iteration){
+    offset = source.rewindBuffer.iterations - iteration;  
+  }
   source.read(offset, (samples) => {
+    if (limit && frameCount >= limit){
+      isConnected = false; // force disconnect if we are beyond a provided limit of seconds
+    }
+    frameCount++;
     res.write(Buffer.from(samples), 'binary');
     return isConnected;
   });
@@ -52,17 +66,6 @@ app.get('/stream/:type', function (req, res) {
 });
 
 
+
 httpsServer.listen(3003);
-
-
-
-
-
-
-
-
-
-
-
-
 
